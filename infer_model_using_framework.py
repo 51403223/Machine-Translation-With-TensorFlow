12b8da6c47f6a1c3f4f7aedcd53314c736f01713
Thanh Long Tran <tranthanhlong3103@gmail.com>
2018-07-13 19:02:33 +0700

    complete infer_model but need more testing

--- infer_model_using_framework.py
+++ infer_model_using_framework.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from utils import embedding
 import os
-import time
 import numpy as np
 
 eos_vocab_id = 0
@@ -18,7 +17,7 @@ def create_dataset(sentences_as_ids):
     return dataset
 
 
-def train_model():
+def test_model():
     print('Loading word embeddings...')
     data_path = 'data/'  # path of data folder
     embeddingHandler = embedding.Embedding()
@@ -64,31 +63,30 @@ def train_model():
 
     ################## create dataset ######################
     batch_size = 64
-    num_epochs = 12
     print('Creating dataset...')
-    print('Number of training examples: ', training_size)
+    print('Number of test examples: ', training_size)
 
     # create training set for encoder (source)
     sentences_src_as_ids = embeddingHandler.convert_sentences_to_ids(dic_src, sentences_src)
     for sentence in sentences_src_as_ids:  # add <eos>
         sentence.append(eos_vocab_id)
-    train_set_src = create_dataset(sentences_src_as_ids)
-    train_set_src_len = create_dataset([[len(s)] for s in sentences_src_as_ids])
+    test_set_src = create_dataset(sentences_src_as_ids)
+    test_set_src_len = create_dataset([[len(s)] for s in sentences_src_as_ids])
 
     # create training set for decoder (target)
     sentences_tgt_as_ids = embeddingHandler.convert_sentences_to_ids(dic_tgt, sentences_tgt)
     # for sentence_as_ids in sentences_tgt_as_ids:  # add </s> id to the end of each sentence of target language
     #     sentence_as_ids.append(eos_vocab_id)
-    train_set_tgt = create_dataset(sentences_tgt_as_ids)
-    train_set_tgt_len = create_dataset([[len(sentence)+1] for sentence in sentences_tgt_as_ids])
+    test_set_tgt = create_dataset(sentences_tgt_as_ids)
+    test_set_tgt_len = create_dataset([[len(sentence)+1] for sentence in sentences_tgt_as_ids])
     # Note: [len(sentence)+1] for later <sos>/<eos>
-    train_set_tgt_padding = create_dataset([np.ones(len(sentence)+1, np.float32) for sentence in sentences_tgt_as_ids])
+    test_set_tgt_padding = create_dataset([np.ones(len(sentence)+1, np.float32) for sentence in sentences_tgt_as_ids])
     ## padding matrix
     # target_weights = create_dataset([np.ones(len(sentence) + 1) for sentence in sentences_tgt_as_ids])
 
     # create dataset contains both previous training sets
-    train_dataset = tf.data.Dataset.zip((train_set_src, train_set_tgt, train_set_src_len, train_set_tgt_len, train_set_tgt_padding))
-    train_dataset = train_dataset.shuffle(buffer_size=training_size, seed=9)
+    train_dataset = tf.data.Dataset.zip((test_set_src, test_set_tgt, test_set_src_len, test_set_tgt_len, test_set_tgt_padding))
+    # train_dataset = train_dataset.shuffle(buffer_size=training_size, seed=9)
     # train_dataset = train_dataset.shuffle(buffer_size=training_size)
     train_dataset = train_dataset.apply(
         tf.contrib.data.padded_batch_and_drop_remainder(batch_size, ([None], [None], [1], [1], [None])))
@@ -129,7 +127,8 @@ def train_model():
 
     # ----------decoder
     encode_output_size = hidden_size*2
-    decode_seq_lens = tf.reshape(len_ys, shape=[batch_size])
+    # decode_seq_lens = tf.reshape(len_ys, shape=[batch_size])
+    decode_seq_lens = encode_seq_lens * 2  # maximum iterations
     attention_output_size = 256
     attention_mechanism = tf.contrib.seq2seq.LuongAttention(
         num_units=encode_output_size,
@@ -142,20 +141,20 @@ def train_model():
         attention_cell, attention_mechanism,
         attention_layer_size=attention_output_size
     )
-    add_sos = tf.concat([tf.reshape([sos_vocab_id]*batch_size, [batch_size, 1]), y_batch], axis=-1)
+    # add_sos = tf.concat([tf.reshape([sos_vocab_id]*batch_size, [batch_size, 1]), y_batch], axis=-1)
     decoder_initial_state = attention_cell.zero_state(dtype=tf.float32, batch_size=batch_size)
     decoder_initial_state = decoder_initial_state.clone(cell_state=enc_2nd_states[-1])
-    dec_outputs, _ = tf.nn.dynamic_rnn(
-        cell=attention_cell,
-        inputs=tf.nn.embedding_lookup(embedding_tgt, tf.transpose(add_sos)),
-        initial_state=decoder_initial_state,
-        sequence_length=decode_seq_lens,
-        dtype=tf.float32,
-        swap_memory=True,
-        time_major=True
-    )
+    # dec_outputs, _ = tf.nn.dynamic_rnn(
+    #     cell=attention_cell,
+    #     inputs=tf.nn.embedding_lookup(embedding_tgt, tf.transpose(add_sos)),
+    #     initial_state=decoder_initial_state,
+    #     sequence_length=decode_seq_lens,
+    #     dtype=tf.float32,
+    #     swap_memory=True,
+    #     time_major=True
+    # )
 
-    # -----------calculate score
+    # projection
     tgt_vocab_size = len(vocab_tgt)
     weight_score = tf.Variable(
         tf.random_uniform(shape=[attention_output_size, tgt_vocab_size], minval=-0.1, maxval=0.1)
@@ -163,85 +162,71 @@ def train_model():
     bias_score = tf.Variable(
         tf.zeros([batch_size, tgt_vocab_size])
     )
-    dec_outputs_len = tf.shape(dec_outputs)[0]
-    def cond(i, *_):
-        return tf.less(i, dec_outputs_len)
-    def body(i, _logits):
-        score = tf.add(
-            tf.matmul(dec_outputs[i], weight_score), bias_score
-        )
-        return i+1, _logits.write(i, score)
-    _, logits = tf.while_loop(
-        cond, body, loop_vars=[0, tf.TensorArray(tf.float32, size=dec_outputs_len, clear_after_read=True)], swap_memory=True
-    )
-    labels = tf.transpose(tf.concat([y_batch, tf.reshape([eos_vocab_id]*batch_size, [batch_size, 1])], axis=-1))
 
-    # ----------loss
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits.stack())  # [time,batch]
-    apply_penalty = cross_entropy * tf.transpose(tf.cast(padding_mask, tf.float32))
-    loss = tf.reduce_sum(apply_penalty) / batch_size
+    # infer
+    def loop_fn(time, cell_output, cell_state, loop_state):
+        elements_finished = time >= decode_seq_lens
+        finished = tf.reduce_all(elements_finished)
+        if cell_output is None:  # time = 0
+            next_cell_state = decoder_initial_state
+            next_input = tf.nn.embedding_lookup(embedding_tgt, [sos_vocab_id]*batch_size)
+            emit_output = tf.constant(0)  # https://github.com/hanxiao/hanxiao.github.io/issues/8
+        else:
+            next_cell_state = cell_state
+            score = tf.add(
+                tf.matmul(cell_output, weight_score), bias_score
+            )
+            softmax = tf.nn.softmax(score)
+            predict = tf.argmax(softmax, axis=-1, output_type=tf.int32)
+            next_input = tf.cond(
+                finished,
+                lambda: tf.nn.embedding_lookup(embedding_tgt, [eos_vocab_id]*batch_size),
+                lambda: tf.nn.embedding_lookup(embedding_tgt, predict)
+            )
+            emit_output = predict
 
-    # ----------optimizer
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-    params = tf.trainable_variables()
-    gradients = tf.gradients(loss, params)  # derivation of loss by params
-    max_gradient_norm = 5
-    clipped_gradients, _ = tf.clip_by_global_norm(gradients, max_gradient_norm)
-    starting_rate = 1.0
-    decay_epochs = 4  # decay learning rate on every n epochs exclude first n epochs
-    decay_step = (training_size // batch_size) * decay_epochs  # num_step_in_single_epoch * n
-    learning_rate = tf.train.exponential_decay(learning_rate=starting_rate, global_step=global_step,
-                                               decay_steps=decay_step, decay_rate=0.1, staircase=True)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    optimizer = optimizer.apply_gradients(zip(clipped_gradients, params), global_step=global_step)
+        next_loop_state = None
+        return elements_finished, next_input, next_cell_state, emit_output, next_loop_state
+
+    outputs_ta, final_state, _ = tf.nn.raw_rnn(attention_cell, loop_fn)
+    outputs = outputs_ta.stack()
+    # -----------calculate score
+
+    # dec_outputs_len = tf.shape(dec_outputs)[0]
+    # def cond(i, *_):
+    #     return tf.less(i, dec_outputs_len)
+    # def body(i, _logits):
+    #     score = tf.add(
+    #         tf.matmul(dec_outputs[i], weight_score), bias_score
+    #     )
+    #     return i+1, _logits.write(i, score)
+    # _, logits = tf.while_loop(
+    #     cond, body, loop_vars=[0, tf.TensorArray(tf.float32, size=dec_outputs_len, clear_after_read=True)], swap_memory=True
+    # )
+    # labels = tf.transpose(tf.concat([y_batch, tf.reshape([eos_vocab_id]*batch_size, [batch_size, 1])], axis=-1))
+    #
 
     #################### train ########################
     log_frequency = 100
     model_path = "./checkpoint_framework/model"
     checkpoint_path = "./checkpoint_framework"
-    loss_epochs = tf.TensorArray(tf.float32, size=num_epochs, dynamic_size=True)
-    training_epoch = tf.Variable(0, trainable=False, name='training_epoch')
     saver = tf.train.Saver()
     with tf.Session() as sess:
-        try:
-            saver.restore(sess, tf.train.latest_checkpoint(checkpoint_dir=checkpoint_path))
-            print('...............Restored from checkpoint_framework')
-        except:
-            sess.run(tf.global_variables_initializer())
-        start_epoch = sess.run(training_epoch)
-        for epoch in range(start_epoch, num_epochs):
-            print('Training epoch', epoch + 1)
-            start_time = time.time()
-            total_loss = 0
-            sess.run(train_iter.initializer)
-            while True:
-                try:
-                    _, l, lr, step = sess.run([optimizer, loss, learning_rate, global_step])
-                    total_loss += l
-                    if np.isnan(l):
-                        return False
-                    print('Step {0}: loss={1} lr={2}'.format(step, l, lr))
-                    # if step % log_frequency == 0:
-                    #     print('Step {0}: loss={1} lr={2}'.format(step, l, lr))
-                except tf.errors.OutOfRangeError:
-                    avg_loss = total_loss / (training_size // batch_size)
-                    loss_epochs = loss_epochs.write(epoch, tf.cast(avg_loss, tf.float32))  # write average loss of epoch
-                    sess.run(training_epoch.assign(epoch + 1))  # starting epoch if restore
-                    saver.save(sess, model_path, epoch)
-                    print('Average loss=', avg_loss)
-                    break
+        saver.restore(sess, tf.train.latest_checkpoint(checkpoint_dir=checkpoint_path))
+        print('...............Restored from checkpoint_framework')
+        sess.run(train_iter.initializer)
+        while True:
+            try:
+                predictions, labels = sess.run([outputs, y_batch])
 
-            print('Epoch {} train in {} minutes'.format(epoch + 1, (time.time() - start_time) / 60.0))
-            print('------------------------------------')
+            except tf.errors.OutOfRangeError:
+                break
 
-        loss_summary = sess.run(loss_epochs.stack())
-        loss_epochs.close()
-        np.savetxt(checkpoint_path + '/loss_summary.txt', loss_summary, fmt='%10.5f')
-        return True
 
-train_model()
+test_model()
 # # train loop prevents 'nan' occurs
 # while True:
 #     train_result = train_model()
 #     if train_result is True:
 #         break
+
