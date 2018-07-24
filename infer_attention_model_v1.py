966ffb82fe2b769f2936909aa0c2e206e55b075c
Thanh Long Tran <tranthanhlong3103@gmail.com>
2018-07-24 15:40:57 +0700

    finish beam search

--- infer_attention_model_v1.py
+++ infer_attention_model_v1.py
@@ -3,6 +3,9 @@ from utils import embedding
 import os
 import numpy as np
 import bleu
+from beam_search import raw_rnn_for_beam_search
+from beam_search import extract_from_tree
+from beam_search import get_word_ids
 
 eos_vocab_id = 0
 sos_vocab_id = 2
@@ -18,7 +21,7 @@ def create_dataset(sentences_as_ids):
     return dataset
 
 
-def test_model(model_path, src_file_name, tgt_file_name):
+def test_model(model_path, src_file_name, tgt_file_name, beam_width=1):
     infer_graph = tf.Graph()
     with infer_graph.as_default():
         data_path = 'data/'  # path of data folder
@@ -66,21 +69,15 @@ def test_model(model_path, src_file_name, tgt_file_name):
 
         # create training set for decoder (target)
         sentences_tgt_as_ids = embeddingHandler.convert_sentences_to_ids(dic_tgt, sentences_tgt)
-        # for sentence_as_ids in sentences_tgt_as_ids:  # add </s> id to the end of each sentence of target language
-        #     sentence_as_ids.append(eos_vocab_id)
         test_set_tgt = create_dataset(sentences_tgt_as_ids)
         test_set_tgt_len = create_dataset([[len(sentence) + 1] for sentence in sentences_tgt_as_ids])
         # Note: [len(sentence)+1] for later <sos>/<eos>
         test_set_tgt_padding = create_dataset(
             [np.ones(len(sentence) + 1, np.float32) for sentence in sentences_tgt_as_ids])
-        ## padding matrix
-        # target_weights = create_dataset([np.ones(len(sentence) + 1) for sentence in sentences_tgt_as_ids])
 
         # create dataset contains both previous training sets
         train_dataset = tf.data.Dataset.zip(
             (test_set_src, test_set_tgt, test_set_src_len, test_set_tgt_len, test_set_tgt_padding))
-        # train_dataset = train_dataset.shuffle(buffer_size=training_size, seed=9)
-        # train_dataset = train_dataset.shuffle(buffer_size=training_size)
         train_dataset = train_dataset.apply(
             tf.contrib.data.padded_batch_and_drop_remainder(batch_size, ([None], [None], [1], [1], [None])))
         train_iter = train_dataset.make_initializable_iterator()
@@ -100,7 +97,6 @@ def test_model(model_path, src_file_name, tgt_file_name):
             dtype=tf.float32
         )  # [batch, time, hid]
         fw_enc_1st_hid_states, bw_enc_1st_hid_states = enc_1st_outputs
-        # fw_enc_1st_last_hid, bw_enc_1st_last_hid = enc_1st_states
 
         # ----------encoder second layer
         num_layers = 2
@@ -118,7 +114,6 @@ def test_model(model_path, src_file_name, tgt_file_name):
 
         # ----------decoder
         encode_output_size = hidden_size * 2
-        # decode_seq_lens = tf.reshape(len_ys, shape=[batch_size])
         decode_seq_lens = encode_seq_lens * 2  # maximum iterations
         attention_output_size = 256
         attention_mechanism = tf.contrib.seq2seq.LuongAttention(
@@ -132,7 +127,6 @@ def test_model(model_path, src_file_name, tgt_file_name):
             attention_cell, attention_mechanism,
             attention_layer_size=attention_output_size
         )
-        # add_sos = tf.concat([tf.reshape([sos_vocab_id]*batch_size, [batch_size, 1]), y_batch], axis=-1)
         decoder_initial_state = attention_cell.zero_state(dtype=tf.float32, batch_size=batch_size)
         decoder_initial_state = decoder_initial_state.clone(cell_state=enc_2nd_states[-1])
 
@@ -145,56 +139,114 @@ def test_model(model_path, src_file_name, tgt_file_name):
             tf.zeros([batch_size, tgt_vocab_size])
         )
 
-        # infer
-        def loop_fn(time, cell_output, cell_state, loop_state):
+        # beam search
+        def loop_fn(time, cell_output, cell_state, log_probs, beam_finished):
             elements_finished = time >= decode_seq_lens  # finish by sentence length
-            if cell_output is None:  # time = 0
-                next_cell_state = decoder_initial_state
-                next_input = tf.nn.embedding_lookup(embedding_tgt, [sos_vocab_id] * batch_size)
-                emit_output = tf.constant(0)  # https://github.com/hanxiao/hanxiao.github.io/issues/8
+            if cell_output is None:  # initialize step
+                next_cell_state = tuple(decoder_initial_state for _ in range(beam_width))
+                next_input = tuple(
+                    tf.nn.embedding_lookup(embedding_tgt, [sos_vocab_id] * batch_size) for _ in range(beam_width))
+                predicted_ids = tf.convert_to_tensor([0] * beam_width)  # https://github.com/hanxiao/hanxiao.github.io/issues/8
+                new_log_probs = tf.zeros([batch_size, beam_width])
+                new_beam_finished = tf.fill([batch_size, beam_width], value=False)
+                parent_indexs = None
             else:
-                next_cell_state = cell_state
-                score = tf.add(
-                    tf.matmul(cell_output, weight_score), bias_score
-                )
-                softmax = tf.nn.softmax(score)
-                predict = tf.argmax(softmax, axis=-1, output_type=tf.int32)
-                elements_finished = tf.logical_or(elements_finished,
-                                                  tf.equal(predict, eos_vocab_id))  # or finish by generated <eos>
-                finished = tf.reduce_all(elements_finished)
-                next_input = tf.cond(
-                    finished,
-                    lambda: tf.nn.embedding_lookup(embedding_tgt, [eos_vocab_id] * batch_size),
-                    lambda: tf.nn.embedding_lookup(embedding_tgt, predict)
-                )
-                emit_output = predict
+                def not_time_0():
+                    next_cell_state = cell_state
+                    # find predicted_ids
+                    values_list = []
+                    indices_list = []
+                    for i in range(beam_width):
+                        score = tf.add(
+                            tf.matmul(cell_output[i], weight_score), bias_score
+                        )
+                        softmax = tf.nn.softmax(score)
+                        log_prob = tf.log(softmax)
+                        values, indices = tf.nn.top_k(log_prob, beam_width,
+                                                      sorted=False)  # [batch, beam], [batch, beam]
+                        # Note: indices is ids of words as well
+                        values = tf.add(values, tf.expand_dims(log_probs[:, i], -1))  # sum with previous log_prob
+                        values_list.append(values)
+                        indices_list.append(indices)
+                    concat_vlist = tf.concat(tf.unstack(values_list, axis=0),
+                                             axis=-1)  # [batch_size, beam_width*beam_width]
+                    concat_ilist = tf.concat(tf.unstack(indices_list, axis=0), axis=-1)
+                    top_values, index_in_vlist = tf.nn.top_k(concat_vlist, beam_width,
+                                                             sorted=False)  # [batch_size, beam_width]
+                    # Note: in tf.nn.top_k, if sorted=False then it's values will be SORTED ASCENDING
 
-            next_loop_state = None
-            return elements_finished, next_input, next_cell_state, emit_output, next_loop_state
+                    predicted_ids = get_word_ids(index_in_vlist, concat_ilist, batch_size)
+                    predicted_ids = tf.stack(predicted_ids)  # [batch_size, beam_width]
 
-        outputs_ta, final_state, _ = tf.nn.raw_rnn(attention_cell, loop_fn)
-        outputs = outputs_ta.stack()  # [time, batch]
-        outputs = tf.transpose(outputs)  # reshape to [batch, time]
-        # -----------calculate score
+                    new_beam_finished = tf.logical_or(tf.equal(predicted_ids, eos_vocab_id), beam_finished)
 
-        # dec_outputs_len = tf.shape(dec_outputs)[0]
-        # def cond(i, *_):
-        #     return tf.less(i, dec_outputs_len)
-        # def body(i, _logits):
-        #     score = tf.add(
-        #         tf.matmul(dec_outputs[i], weight_score), bias_score
-        #     )
-        #     return i+1, _logits.write(i, score)
-        # _, logits = tf.while_loop(
-        #     cond, body, loop_vars=[0, tf.TensorArray(tf.float32, size=dec_outputs_len, clear_after_read=True)], swap_memory=True
-        # )
-        # labels = tf.transpose(tf.concat([y_batch, tf.reshape([eos_vocab_id]*batch_size, [batch_size, 1])], axis=-1))
-        #
+                    # find parent_ids that match word_ids_to_add
+                    parent_indexs = index_in_vlist // beam_width
+                    # find new_log_probs
+                    new_log_probs = top_values
+                    # define next_input
+                    finished = tf.reduce_all(elements_finished)
+                    next_input = tuple(
+                        tf.cond(
+                            finished,
+                            lambda: tf.nn.embedding_lookup(embedding_tgt, [eos_vocab_id] * batch_size),
+                            lambda: tf.nn.embedding_lookup(embedding_tgt, predicted_ids[:, i])
+                        ) for i in range(beam_width)
+                    )
+
+                    return elements_finished, next_input, next_cell_state, predicted_ids, new_log_probs, new_beam_finished, parent_indexs
+
+                def time_0():
+                    next_cell_state = cell_state
+                    # find next_input
+                    score = tf.add(
+                        tf.matmul(cell_output[0], weight_score), bias_score
+                    )
+                    softmax = tf.nn.softmax(score)
+                    log_prob = tf.log(softmax)
+                    top_values, predicted_ids = tf.nn.top_k(log_prob, beam_width,
+                                                            sorted=False)  # [batch_size, beam_width]
+
+                    new_beam_finished = beam_finished
+
+                    parent_indexs = tf.fill([batch_size, beam_width], value=-1)
+
+                    new_log_probs = log_probs
+
+                    finished = tf.reduce_all(elements_finished)
+                    next_input = tuple(
+                        tf.cond(
+                            finished,
+                            lambda: tf.nn.embedding_lookup(embedding_tgt, [eos_vocab_id] * batch_size),
+                            lambda: tf.nn.embedding_lookup(embedding_tgt, predicted_ids[:, i])
+                        ) for i in range(beam_width)
+                    )
+
+                    return elements_finished, next_input, next_cell_state, predicted_ids, new_log_probs, new_beam_finished, parent_indexs
+
+                # Important note: we won't feed <sos> at step 0 because it will lead to all same results on all beams
+                # instead, we feed top-k predictions generated from feeding <sos> as input
+                # other returns will be pass without change
+                elements_finished, next_input, next_cell_state, predicted_ids, new_log_probs, new_beam_finished, parent_indexs = tf.cond(
+                    tf.equal(time, 0), time_0, not_time_0)
+
+            return elements_finished, next_input, next_cell_state, predicted_ids, new_log_probs, new_beam_finished, parent_indexs
+
+        predicted_ids_ta, parent_ids_ta, penalty_lengths, final_log_probs = raw_rnn_for_beam_search(attention_cell,
+                                                                                                    loop_fn)
+        translation_ta = extract_from_tree(predicted_ids_ta, parent_ids_ta, batch_size, beam_width)
+        outputs = translation_ta.stack()  # [time, batch, beam]
+        # choose best translation with maximum sum log probability
+        normalize_log_probs = final_log_probs / penalty_lengths
+        chosen_translations = tf.argmax(normalize_log_probs, axis=-1, output_type=tf.int32)  # [batch]
+        transpose_outputs = tf.transpose(outputs, perm=[1, 2, 0])  # transpose to [batch, beam, time]
+        final_output = get_word_ids(tf.expand_dims(chosen_translations, -1), transpose_outputs, batch_size)
+        final_output = tf.stack(final_output)  # [batch, 1, time]
+        final_output = tf.reshape(final_output, [batch_size, -1])  # [batch, time]
 
         #################### infer ########################
         saver = tf.train.Saver()
         with tf.Session() as sess:
-            # saver.restore(sess, tf.train.latest_checkpoint(checkpoint_dir=checkpoint_path))
             saver.restore(sess, model_path)
             sess.run(train_iter.initializer)
             references = []
@@ -203,10 +255,8 @@ def test_model(model_path, src_file_name, tgt_file_name):
             # third dimension is length of each sentence (maybe differ from each other)
             translation = []
             while True:
-                # for i in range(10):
-                #     print(i)
                 try:
-                    predictions, labels = sess.run([outputs, y_batch])
+                    predictions, labels = sess.run([final_output, y_batch])
                     # perform trimming <eos> to not to get additional bleu score by overlap padding
                     predictions = [np.trim_zeros(predict, 'b') for predict in predictions]
                     labels = [np.trim_zeros(lb, 'b') for lb in labels]
@@ -222,3 +272,6 @@ def test_model(model_path, src_file_name, tgt_file_name):
             reshaped_references = [[ref] for ref in references]
             bleu_score, *_ = bleu.compute_bleu(reshaped_references, translation, max_order=4, smooth=False)
             return bleu_score
+
+# bleu = test_model(model_path='checkpoint_v1/model-11', src_file_name='tst2012.vi', tgt_file_name='tst2012.en', beam_width=1)
+# print(bleu*100)
